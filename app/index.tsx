f9f3b7e22b723077e86052f4f35efd5408d0a522
Albert Sigsbert <ishengomaalbert@gmail.com>
2024-09-06 08:48:10 +0300

    Setup NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,14 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet,Text,View } from "react-native";
+import { Text,View } from "react-native";
 
 export default function App(){
     return (
-        <View style={styles.container}>
-            <Text>Artifex!</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Artifex!</Text>
             <StatusBar style="auto" />
             <Link href="/profile">Go to profile</Link>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
