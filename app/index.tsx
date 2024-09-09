6133c2bd9edc6a6529cbf073e27127fae48c7688
Albert Sigsbert <ishengomaalbert@gmail.com>
2024-09-09 09:43:14 +0300

    Load fonts and Assets

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Text,View } from "react-native";
 export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Artifex!</Text>
+            <Text className="text-3xl font-pblack">Artifex!</Text>
             <StatusBar style="auto" />
             <Link href="/profile">Go to profile</Link>
         </View>
